2d4c856a2523e03b3ac1c7307e3d542133d3a857
Javier <65590523+javierc435@users.noreply.github.com>
2020-10-06 23:33:50 +0200

    Add devise feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     if params[:category].blank?
       @posts = Post.all.order("created_at DESC")
