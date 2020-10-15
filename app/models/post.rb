70a4a83f92faf2a8cfe8717ffe64d372b04362fd
Javier <65590523+javierc435@users.noreply.github.com>
2020-10-15 09:45:26 +0200

    Add total and order by total to services and suggestions pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,22 @@ class Post < ApplicationRecord
   before_save :calculate_total
 
   def calculate_total
-    sum_value = number + social_media + phone_contact
+    if phone_contact == nil && social_media == nil
+      sum_value = number
+    elsif phone_contact == nil && number == nil
+      sum_value = social_media
+    elsif number == nil && social_media == nil
+      sum_value = phone_contact
+    elsif number == nil
+      sum_value = social_media + phone_contact
+    elsif social_media == nil
+      sum_value = number + phone_contact
+    elsif phone_contact == nil
+      sum_value = number + social_media
+    else
+      sum_value = number + social_media + phone_contact
+    end
+
     self.total = sum_value
   end
 end
